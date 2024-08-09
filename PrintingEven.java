9b515e258fc85ddd52b8655949f96f39cf445520
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2024-08-09 12:53:17 +0530

    string_logicals1

--- PrintingEven.java
+++ PrintingEven.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
 public class PrintingEven{  
 // Write the Code to printEven Length Words With  their Length
+public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    System.out.println("enter a string sentence");
+    String a=s.nextLine();
+    for(String i:a.split(" ")){
+        if(i.length()%2==0){
+            System.out.println(i);
+
+        }
+    }
+
+}
 }
