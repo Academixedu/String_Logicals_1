72eb29e782d600a913ad267c7c1df115b0b68141
ganeshteki <ganeshteki2001@gmail.com>
2024-08-09 14:14:08 +0530

    strings

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,19 @@
 public class Palindrome{
 // prepare a Code to findout whether a String is Palindrome or not
+public static Boolean reverse(String s){
+    String rev="";
+    boolean a=false;
+    for(int i=s.length()-1;i>=0;i--){
+        rev=rev+s.charAt(i);
+    }
+    if(s.equals(rev)){
+        a=true;
+        System.out.println("palindrome");
+    }
+    return a;
+}
+public static void main(String args[]){
+    String s="YAY";
+   System.out.println(reverse(s));
+}
 }
--- PrintingEven.java
+++ PrintingEven.java
@@ -1,3 +1,25 @@
 public class PrintingEven{  
 // Write the Code to printEven Length Words With  their Length
+public static void dynamic(String s){
+String words[]=s.split(" ");
+int []wordslength=new int[words.length];
+for(int i=0;i<=words.length;i++){
+    wordslength[i]=words[i].length();
+    if(wordslength[i]%2==0){
+        System.out.println(words[i]+"length is "+wordslength[i]);
+    }
+}
+}
+public static void statical(String s){
+    for(String word:s.split(" ")){
+        if(word.length()%2==0){
+          System.out.println(word+""+word.length());
+        }
+    }
+}
+public static void main(String args[]){
+    String s="he is a pilot";
+    dynamic(s);
+    statical(s);
+}
 }
