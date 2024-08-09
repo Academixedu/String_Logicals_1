e9c212823d8a776b6b7d740c6bdbd21c1ade3fdf
syamkarthik0 <dharmavarapukarthik@gmail.com>
2024-08-09 14:46:41 +0530

    From karthik

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class Palindrome{
-// prepare a Code to findout whether a String is Palindrome or not
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String: ");
+        String str =sc.nextLine();
+        String rev="";
+        for(int i=str.length()-1;i>=0;i--){
+                rev+=str.charAt(i);
+                if(str.equals(rev)){
+                    System.out.println("The Given String is Palindrome: "+rev);
+                }
+            }
+             if(!str.equals(rev)){
+                System.out.println("The Given String is not a Palindrome: "+str);
+            }
+    }
 }
