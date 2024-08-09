6cedeb60aa4b5d3cf8376cb5019db0ef15f8bad0
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-08-09 13:59:02 +0530

    done

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
 public class Palindrome{
 // prepare a Code to findout whether a String is Palindrome or not
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter your string");
+    String n=sc.nextLine();
+    String rev="";
+      for(int i=n.length()-1;i>=0;i--){
+    rev =rev+n.charAt(i);
+}
+    if(n.equals(rev)){
+    System.out.println("Given String is a palindrome:");
+    }
+    else{
+        System.out.println("Given String is a not a palindrome");
+    }
+}
 }
--- PrintingEven.java
+++ PrintingEven.java
@@ -1,3 +1,22 @@
-public class PrintingEven{  
+ import java.util.Scanner;
+ public class PrintingEven{  
 // Write the Code to printEven Length Words With  their Length
+ public static void dynamic(String s){
+    String [] s_words=s.split(" ");
+    int s_length[]=new int[s_words.length];
+    for(int i=0;i<s_words.length;i++){
+            s_length[i]=s_words[i].length();
+            if(s_length[i]%2==0){
+                System.out.println(s_words[i]+ " "+s_words[i].length());
+            }
+    }
+
+ }
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("give your sentence");
+    String s=sc.nextLine();
+    PrintingEven p=new PrintingEven();
+    p.dynamic(s);
+}
 }
