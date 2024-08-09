76cc6ad1dc6dee4668c0bab950dfc6cdd69097a8
sujatha-94 <sujathakarri701@gmail.com>
2024-08-09 16:38:28 +0530

    String

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,24 @@
 public class Palindrome{
 // prepare a Code to findout whether a String is Palindrome or not
+    public static boolean pal(String str)
+    {
+        String rev="";
+        boolean ans=false;
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            rev+=str.charAt(i);
+        }
+        if(str.equals(rev))
+        {
+            ans=true;
+            System.out.println("Palindrome");
+        }
+        return ans;
+        
+    }
+    public static void main(String[] args)
+    {
+        String s="SOS";
+        System.out.println(pal(s));
+    }
 }
--- PrintingEven.java
+++ PrintingEven.java
@@ -1,3 +1,17 @@
 public class PrintingEven{  
 // Write the Code to printEven Length Words With  their Length
+public class Main {
+    public static void main(String[] args)
+    {
+        String str = "Hello World Java Programming";
+        String[] words = str.split(" ");
+        for (String word : words) {
+            int length = word.length();
+            if (length % 2 == 0 && length > 0)
+            {
+                System.out.printf("%s - Length: %d%n", word, length);
+            }
+        }
+    }
+}
 }
