946b5a72bad717f9860d35dae67ba949406f0775
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-09 15:29:11 +0530

    Update PrintingEven.java

--- PrintingEven.java
+++ PrintingEven.java
@@ -1,3 +1,20 @@
 public class PrintingEven{  
+  import java.util.Scanner;
 // Write the Code to printEven Length Words With  their Length
+public class printEven {  
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+        
+        String[] words = input.split(" ");
+
+        for (String word : words) {
+            if (word.length() % 2 == 0) {
+                System.out.println(word + " (Length: " + word.length() + ")");
+            }
+        }
+        scanner.close();
+    }
 }
