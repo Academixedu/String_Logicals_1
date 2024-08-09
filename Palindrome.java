ffadffa73670557490cc808b4e12a1611bb79694
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-08-09 14:12:24 +0530

    string

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,22 @@
 public class Palindrome{
 // prepare a Code to findout whether a String is Palindrome or not
+public static Boolean rev(String s){
+    String rev="";
+    boolean a=false;
+    for(int i=s.length()-1;i>=0;i-- ){
+        rev=rev+s.charAt(i);
+
+    }
+    if(s.equals(rev)){
+        System.out.println("palindrome");
+        a=true;
+    
+    }
+    return a;
+   
+}
+public static void main(String[] args) {
+    String s="wow";
+    System.out.println(rev(s));
+}
 }
