829fa95f43bc54e1fa661aa55a03ac78e130c056
saikumar34892 <saikumarmatta3821@gmail.com>
2024-08-09 15:27:47 +0530

    from sai

--- PrintingEven.java
+++ PrintingEven.java
@@ -1,3 +1,20 @@
 public class PrintingEven{  
 // Write the Code to printEven Length Words With  their Length
+public static void main(String[] args) {
+    String a="my self sai";
+    String words[]=a.split(" ");
+    int length[]=new int [words.length];
+    for(int i=0;i<words.length;i++){
+        length[i]=words[i].length();
+    }
+    for(int i=0;i<words.length;i++){
+        if(length[i]%2==0){
+            System.out.println(+length[i]+ " it's even :"+words[i]);
+        }else{
+            System.out.println(+length[i]+ " it's not even :"+words[i]);
+        }
+    }
+
+
+}
 }
