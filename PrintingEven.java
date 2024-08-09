00e493a956e8d37c8ebdceda985601c8679e9995
kavitha-guntru <kavithaguntru60@gmail.com>
2024-08-09 16:03:32 +0530

    KAVITHA

--- PrintingEven.java
+++ PrintingEven.java
@@ -1,3 +1,31 @@
-public class PrintingEven{  
-// Write the Code to printEven Length Words With  their Length
-}
+public class PrintingEven {
+    public void dynamic(String s) {
+        //Splitting
+        String []words=s.split(" ");
+        //Feeding Each word into one Array	
+        int[]wordsLength=new int[words.length];
+        
+        for(int i=0;i<words.length;i++) {
+            wordsLength[i]=words[i].length();
+        }
+        for(int i=0;i<words.length;i++) {
+            if(wordsLength[i]%2==0) {
+                System.out.println(words[i]+" "+words[i].length());
+            }
+        }
+    }
+    public void statical(String s) {
+        for(String word:s.split(" ")) {
+            if(word.length()%2==0) {
+                System.out.println(word+" "+word.length());
+            }
+        }
+    }
+        
+        public static void main(String[] args) {
+        System.out.println("Even Length words");
+        String s="Oppenhimmer introduced Atomic Bomb";
+       PrintingEven p=new PrintingEven();
+       p.dynamic(s);
+    }
+    }
