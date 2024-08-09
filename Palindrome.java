86b023b1edb384a3bec4f363fda29f9ea34641eb
kavitha-guntru <kavithaguntru60@gmail.com>
2024-08-09 16:07:04 +0530

    KAVITHA

--- Palindrome.java
+++ Palindrome.java
@@ -1,19 +1,21 @@
 public class Palindrome {
     public static boolean pal(String str) {
-        String rev="";
-        boolean ans=false;
-        for(int i=str.length()-1;i>=0;i--) {
-            rev+=str.charAt(i);
+        String rev = "";
+        boolean ans = false;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
         }
-        if(str.equals(rev)) {
-            ans=true;
+        if (str.equals(rev)) {
+            ans = true;
             System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
         }
         return ans;
-        
     }
+
     public static void main(String[] args) {
-        String s="SOS";
+        String s = "SOP";
         System.out.println(pal(s));
     }
-    }
+}
